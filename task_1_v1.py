8e0824806bd5cc3f82c5ecd05d216840cc3b2fe1
Lelik <lpk_87@bk.ru>
2026-03-18 21:02:00 +0300

    Do first task shorter

---
+++ task_1_v1.py
@@ -0,0 +1,48 @@
+class Case:
+    def __init__(self, test_case_id, name, step_description, expected_result):
+        self.test_case_id = test_case_id
+        self.name = name
+        self.step_description = step_description
+        self.expected_result = expected_result
+
+    def print_test_case_info(self):
+        print(
+            f"ID тест-кейса:  {self.test_case_id}"
+            f"\nНазвание: {self.name}"
+            f"\nОписание шага: {self.step_description}"
+            f"\nОжидаемый результат: {self.expected_result}"
+        )
+
+
+class ExtendedCase(Case):
+    def __init__(
+        self,
+        test_case_id,
+        name,
+        step_description,
+        expected_result,
+        precondition,
+        environment,
+    ):
+        super().__init__(test_case_id, name, step_description, expected_result)
+        self.precondition = precondition
+        self.environment = environment
+
+    # Метод подкласса сокращен до возможного для меня максимума.
+    # При этом в конце вывода присутствует None, т.к. родительский класс ничего не возвращает.
+    # Проблема с выводом None решается только изменением родительского класса, решение представлено в файле task_1_v2.py
+    def print_test_case_info(self):
+        print(
+            f"Предусловие: {self.precondition} \nОкружение: {self.environment} {super().print_test_case_info()}"
+        )
+
+
+case = ExtendedCase(
+    "1",
+    "Наличие кнопки Принять",
+    "1. Открыть вкладку приёма документов 2. Проверить наличие кнопки ",
+    "Кнопка доступна",
+    "Открыть сервис",
+    "Яндекс Браузер",
+)
+case.print_test_case_info()
---
+++ task_1_v2.py
@@ -0,0 +1,41 @@
+class Case:
+    def __init__(self, test_case_id, name, step_description, expected_result):
+        self.test_case_id = test_case_id
+        self.name = name
+        self.step_description = step_description
+        self.expected_result = expected_result
+
+    # Вариант с переписанным родительским классом, для решения проблемы вывода None после использования родительского класса в подклассе
+    def print_test_case_info(self):
+        return f"ID тест-кейса: {self.test_case_id} \nНазвание: {self.name} \nОписание шага: {self.step_description} \nОжидаемый результат: {self.expected_result}"
+
+
+class ExtendedCase(Case):
+    def __init__(
+        self,
+        test_case_id,
+        name,
+        step_description,
+        expected_result,
+        precondition,
+        environment,
+    ):
+        super().__init__(test_case_id, name, step_description, expected_result)
+        self.precondition = precondition
+        self.environment = environment
+
+    def print_test_case_info(self):
+        print(
+            f"{super().print_test_case_info()} \nПредусловие: {self.precondition} \nОкружение: {self.environment}"
+        )
+
+
+case = ExtendedCase(
+    "1",
+    "Наличие кнопки Принять",
+    "1. Открыть вкладку приёма документов 2. Проверить наличие кнопки ",
+    "Кнопка доступна",
+    "Открыть сервис",
+    "Яндекс Браузер",
+)
+case.print_test_case_info()
