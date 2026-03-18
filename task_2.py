a434a26468ff3372c6b0436b362f9c146fcf3c05
Lelik <lpk_87@bk.ru>
2026-03-18 22:26:13 +0300

    Do 2 task with super()

--- task_2.py
+++ task_2.py
@@ -10,15 +10,16 @@ class Movies:
 class Comedy(Movies):
     def add_movie(self, movie):
         super().add_movie(movie)
-        return f'Комедии: {self.movies}'
+        return f"Комедии: {self.movies}"
 
 
 class Drama(Movies):
     def add_movie(self, movie):
         super().add_movie(movie)
-        return f'Драмы: {self.movies}' 
+        return f"Драмы: {self.movies}"
+
 
 comedy = Comedy([])
 drama = Drama([])
-print(comedy.add_movie('Большой куш'))
-print(drama.add_movie('Оружейный барон'))
+print(comedy.add_movie("Большой куш"))
+print(drama.add_movie("Оружейный барон"))
